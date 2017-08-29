f75cf27c4e6ea9e6265c0213607b52a63eae2fe3
Himanshu Arora <Himanshu.Arora@iontrading.com>
2017-08-29 10:53:48 +0530

    Add foundation & JQuery

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,11 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+
+
 var name = "Himanshu";
 var message = "Message from component"
 ReactDOM.render(
