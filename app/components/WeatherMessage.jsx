a3e5da9845bd549ee1ac9407897f0fc77ca5bf1b
Himanshu Arora <Himanshu.Arora@iontrading.com>
2017-08-30 11:22:51 +0530

    Add Button Styles

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,19 +1,9 @@
 var React = require('react');
-// var WeatherMessage = React.createClass({
-//
-//   render : function(){
-//   var location =  this.props.location;
-//   var temp = this.props.temp;
-//      return (
-//        <h3> its {temp} in {location} </h3>
-//      )
-//   }
-// });
 
 var WeatherMessage = (props) => {
-  var {temp, location} =  props;  
+  var {temp, location} =  props;
      return (
-       <h3> its {temp} in {location} </h3>
+       <h1 className="text-center">its {temp} in {location} </h1>
      )
   };
 
