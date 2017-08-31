bb859e8f5fc3ff46b81cdacc1ae0486d6bb188da
Himanshu Arora <Himanshu.Arora@iontrading.com>
2017-08-31 13:06:08 +0530

    Update Code

--- app/app.jsx
+++ app/app.jsx
@@ -9,11 +9,9 @@ var Examples = require('Examples');
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css')
 
+
 // App css
 require('style!css!sass!applicationStyles')
-
-
-
 
 ReactDOM.render(
   <Router history={hashHistory}>
--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,23 +1,22 @@
 var React = require('react');
 
 var ErrorModal = React.createClass({
-  getDefaultProps: function(){
-    return{
+  getDefaultProps: function () {
+    return {
       title: 'Error'
     };
   },
-
   propTypes: {
-    title: React.PropTypes.string,
-    message: React.PropTypes.string.isRequired
+      title: React.PropTypes.string,
+      message: React.PropTypes.string.isRequired
   },
-
   componentDidMount: function () {
     var modal = new Foundation.Reveal($('#error-modal'));
     modal.open();
   },
   render: function () {
     var {title, message} = this.props;
+
     return (
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
         <h4>{title}</h4>
